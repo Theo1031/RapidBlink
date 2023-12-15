f7667d85cb6b6e1a95b88b1d41485dc99653da32
Theo1031 <147471513+Theo1031@users.noreply.github.com>
2023-12-14 22:35:43 -0800

    Good verison

--- content.js
+++ content.js
@@ -3,6 +3,7 @@
 function spritzText(text, speed) {
     const words = text.split(/\s+/);
     let currentIndex = 0;
+    let wordSpeed = speed; // To keep track of the speed
 
     // Create the backdrop for text display
     const backdrop = document.createElement('div');
@@ -17,6 +18,31 @@ function spritzText(text, speed) {
     backdrop.style.justifyContent = 'center';
     backdrop.style.alignItems = 'center';
 
+    // Create the speed control bar
+    const speedControl = document.createElement('input');
+    speedControl.type = 'range';
+    speedControl.max = '1000';
+    speedControl.min = '100';
+    speedControl.value = '860'; // Set initial value for +50 faster speed
+    speedControl.style.position = 'absolute';
+    speedControl.style.top = '20px';
+    speedControl.style.left = '50%';
+    speedControl.style.transform = 'translateX(-50%)';
+    speedControl.style.zIndex = '1001';
+
+    // Initial wordSpeed calculation
+    wordSpeed = 1100 - speedControl.value; // Calculate initial wordSpeed
+
+    // Add the event listener for the speed control bar
+    speedControl.addEventListener('input', function() {
+        wordSpeed = 1100 - speedControl.value; // Update wordSpeed on input
+    });
+
+    backdrop.appendChild(speedControl);
+
+
+    
+
     // Create the container for displaying words
     const spritzContainer = document.createElement('div');
     spritzContainer.style.fontSize = '40px';
@@ -24,6 +50,23 @@ function spritzText(text, speed) {
 
     backdrop.appendChild(spritzContainer);
     document.body.appendChild(backdrop);
+
+    // Create the Patreon button
+    const patreonButton = document.createElement('button');
+    patreonButton.textContent = 'Patreon';
+    patreonButton.style.position = 'absolute';
+    patreonButton.style.top = '20px';
+    patreonButton.style.left = '20px';
+    patreonButton.style.padding = '10px';
+    patreonButton.style.fontSize = '20px';
+    patreonButton.style.cursor = 'pointer';
+
+    // Add the event listener for the Patreon button
+    patreonButton.addEventListener('click', function() {
+        window.open('https://www.patreon.com/Tiggum274', '_blank');
+    });
+
+    backdrop.appendChild(patreonButton);
 
     // Create the quit button
     const quitButton = document.createElement('button');
@@ -44,13 +87,40 @@ function spritzText(text, speed) {
     backdrop.appendChild(quitButton);
 
     let updateTimeout; // To keep reference of the timeout
+    // Create the options button
+    const optionsButton = document.createElement('button');
+    optionsButton.textContent = 'Options';
+    optionsButton.style.position = 'absolute';
+    optionsButton.style.bottom = '20px';
+    optionsButton.style.right = '20px';
+    optionsButton.style.padding = '10px';
+    optionsButton.style.fontSize = '20px';
+    optionsButton.style.cursor = 'pointer';
+
+    // Initially hide the Patreon button and the speed control bar
+    patreonButton.style.display = 'none';
+    speedControl.style.display = 'none';
+
+    // Add the event listener for the options button
+    optionsButton.addEventListener('click', function() {
+        // Toggle the visibility of the Patreon button and the speed control bar
+        if (patreonButton.style.display === 'none') {
+            patreonButton.style.display = 'block';
+            speedControl.style.display = 'block';
+        } else {
+            patreonButton.style.display = 'none';
+            speedControl.style.display = 'none';
+        }
+    });
+
+    backdrop.appendChild(optionsButton);
 
     // Function to update the displayed word
     function updateWord() {
         if (currentIndex < words.length) {
             spritzContainer.textContent = words[currentIndex];
             currentIndex++;
-            updateTimeout = setTimeout(updateWord, speed);
+            updateTimeout = setTimeout(updateWord, wordSpeed);
         } else {
             document.body.removeChild(backdrop); // Remove the backdrop after displaying all words
         }
